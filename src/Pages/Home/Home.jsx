5b235c15d2126b646527cb48582b815c4c076b7a
ayoubelhabchi <ayoub.elhabchi.2000@gmail.com>
2024-08-16 13:02:17 +0100

    Adding cards

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.css'
 import Hero from '../../Components/Hero/Hero'
+import TrendingMovies from '../../Components/TrendingMovies/TrendingMovies'
 
 function Home() {
   return (
     <div className='Components_Container'>
       <Hero/>
+      <TrendingMovies/>
     </div>
   )
 }
