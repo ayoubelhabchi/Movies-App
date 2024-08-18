c0c6014b37900855d946b3d564626e9c197275ba
ayoubelhabchi <ayoub.elhabchi.2000@gmail.com>
2024-08-18 22:05:28 +0100

    styling hero slide

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,60 +6,65 @@ import 'react-multi-carousel/lib/styles.css';
 import { FaStar } from "react-icons/fa";
 import { AiFillLike } from "react-icons/ai";
 import getCarouselSettings from '../../tools/carouselSettings';
-function Hero() {
 
+function Hero() {
     const [popularMovies, setPopularMovies] = useState([]);
 
     useEffect(() => {
         fetchPopularMovies()
             .then((popularMovies) => {
-            setPopularMovies(popularMovies);
+                setPopularMovies(popularMovies);
             });
     }, []);
-  
+
     console.log(popularMovies);
 
     const settings = getCarouselSettings();
 
-
     return (
         <div className="Cards_Conainer-Hero">
-        <div className="Main_card-Hero">
-          <Carousel {...settings}>
-            {popularMovies.map((movie, index) => (
-              <div key={index}>
-                <img
-                  className="img"
-                  src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-                  alt={movie.title}
-                />
-                <div className="shadow-background-Hero">
-                  <div className="shadow-overlay-Hero">
-                  <div className="card-info-container-Hero">
-                        <div className="card-info-Hero">
-                            <h1 className="Movie_Title-Hero">{movie.title}</h1>
-                            <div className="ratings-votes-container-Hero">
-                            <div className="rating-Hero">
-                                <FaStar />
-                                <h2>{movie.vote_average}</h2>
+            <div className="Main_card-Hero">
+                <Carousel {...settings}>
+                    {popularMovies.map((movie, index) => (
+                        <div key={index}>
+                            <img
+                                className="img"
+                                src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+                                alt={movie.title}
+                            />
+                            <div className="shadow-background-Hero">
+                                <div className="shadow-overlay-Hero"></div>
                             </div>
-                            <div className="vots-Hero">
-                                <AiFillLike className="AiFillLike" />
-                                <h2>{movie.vote_count}</h2>
-                            </div>
+                            {/* Card Info Container */}
+                            <div className="card-info-container-Hero">
+                                <div className="card-info-Hero">
+                                    <h1 className="Movie_Title-Hero">{movie.title}</h1>
+                                    <div className="ratings-votes-container-Hero">
+                                        <div className="rating-Hero">
+                                            <FaStar />
+                                            <h2>{movie.vote_average}</h2>
+                                        </div>
+                                        <span className="dot-separator-Hero">•</span>
+                                        <div className="vots-Hero">
+                                            <AiFillLike className="AiFillLike" />
+                                            <h2>{movie.vote_count}</h2>
+                                        </div>
+                                        <span className="dot-separator-Hero">•</span>
+                                        <div className="data-Hero">
+                                          <h2>{movie.release_date.substring(0, 4)}</h2>
+                                        </div>
+                                    </div>
+                                        <div className="overview-Hero">
+                                          <p className="text-overview">{movie.overview}</p>
+                                        </div>
+                                </div>
                             </div>
                         </div>
-                        </div>
-
-                  </div>
-                </div>
-              </div>
-            ))}
-          </Carousel>
+                    ))}
+                </Carousel>
+            </div>
         </div>
-      </div>
-    
-  )
+    )
 }
 
-export default Hero
+export default Hero;
