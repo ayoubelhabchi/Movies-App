3955a95e6e8a9c35b167172820520b1ef436fb3b
ayoubelhabchi <ayoub.elhabchi.2000@gmail.com>
2024-09-27 20:45:00 +0100

    fixing loader

--- frontend/src/Components/Loader/Loader.jsx
+++ frontend/src/Components/Loader/Loader.jsx
@@ -3,6 +3,8 @@ import "./Loader.css";
 import "ldrs/hourglass";
 
 function Loader() {
+  console.log("setLoading");
+
   return (
     <div className="loader-container">
       <l-hourglass size="150" speed="1" color="#c01733"></l-hourglass>
